a9c966d63cdb7bb49661155d2c49564427669c0a
judithkoelewijn <j.koelewijn@bisharp.nl>
2022-04-21 17:11:17 +0200

    Finished assignment 2: serve (express, localhost)

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 
 console.log("This is a test with my personal access token");
 
+// test with Hello World //
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
