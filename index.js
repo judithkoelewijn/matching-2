807a2e0f6c43aac4c9bdae134bbbc188716cedee
judithkoelewijn <j.koelewijn@bisharp.nl>
2022-04-28 10:07:50 +0200

    delete node modules

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ console.log("This is a test with my personal access token");
 
 // test with Hello World //
 
-const express = require('express')
-const app = express()
-const port = 3000
+// Load HTTP module
+const http = require("http");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const hostname = "127.0.0.1";
+const port = 8000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// Create HTTP server
+const server = http.createServer(function(req, res) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body "Hello World"
+   res.end('Hello World\n');
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function() {
+   console.log(`Server succesfully running at http://${hostname}:${port}/`);
 })
