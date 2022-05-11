aa6471ea12f60083a1ea55fb4ee64d440f195826
judithkoelewijn <j.koelewijn@bisharp.nl>
2022-05-11 11:43:31 +0200

    finished serve assignment

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 const port = 3000
+const path = require("path")
 app.set('view engine', 'ejs')
+
+/* middleware */ 
+
+app.use('/public', express.static('public'));
 
 const user = {
     firstName: 'Judith',
     lastName: 'Koelewijn',
 }
+
+/* Basic routing: determining how an application responds to a client request */ 
+
 app.get('/', (req, res) => {
     res.render('pages/index', {
         user: user
@@ -23,12 +31,27 @@ app.get('/about', (req, res) => {
 })
 
 
-/* Middleware */ 
+app.get('/login', (req, res) => {
+    res.render('pages/login', )
+ })
+ 
 
- app.use(express.static('public'))
+app.get('/profile', (req, res) => {
+    res.render('pages/profile', )
+ })
 
 
-/* If no routes give response, change route to 404 page (state) */ 
+ app.get('/results', (req, res) => {
+    res.render('pages/results', )
+ })
+
+/* Middleware (serving static files in Express) */ 
+
+app.use(express.static('public'))
+
+
+/* If no routes give response, change route to 404 page (instead of 404 state) */ 
+
 app.use( (req, res) => {
     res.status(404).render('pages/404')
 })
@@ -40,3 +63,4 @@ app.use( (req, res) => {
    console.log(`web server  running on http://localhost:${port}`)
  })
  
+
